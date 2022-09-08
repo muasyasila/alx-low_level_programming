dfdb85892069cd0bcc7ad3007f67cd5ced4c04d3
muasyasila <curtissila4@gmail.com>
2022-09-08 09:20:41 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
