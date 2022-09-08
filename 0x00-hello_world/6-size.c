e7e09f303941dd0654f6dbb38051a55a799c00d0
muasyasila <curtissila4@gmail.com>
2022-09-08 09:43:37 -0700

    Task 6x

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,29 +1,47 @@
 #include <stdio.h>
 
-
-
-/**
+/**                                                                                                                                     
  *
- * main - Entry point
+ *  *                                                                                                                                      
  *
- * Description: using the main function
- * this program prints "Programing is like building a multilingual puzzle"
- * Return: 0
- */
+ *   * main - Entry point                                                                                                                   
+ *
+ *    *                                                                                                                                      
+ *
+ *     * Description: using the main function                                                                                                 
+ *
+ *      * this program prints "Programing is like building a multilingual puzzle"                                                              
+ *
+ *       * Return: 0                                                                                                                            
+ *
+ *        */
+
 
 int main(void)
 
 {
 
 		int a;
+
 			long int b;
-			long long int 
-				char d;
-				float f;
-				
-							printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));								printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));									printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-						printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-								printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+				long long int c;
+
+					char d;
+
+						float f;
+
+
+
+							printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+
+								printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+
+									printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+
+										printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+
+											printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 												return (0);
 
