953d7e99025154baaad366a4ff8896602a8424ae
muasyasila <curtissila4@gmail.com>
2022-09-11 06:48:22 -0700

    Task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print all combination of two digits
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit2 = 0; digit1 < 9; digit++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
