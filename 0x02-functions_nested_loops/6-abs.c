1a9357ca59f63385fa33c3ae6f0c4d6ceb0edc25
muasyasila <curtissila4@gmail.com>
2022-09-14 01:21:28 -0700

    Task X

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * _abs - function that computes the absolute value of an integer
- * @c:  is the int that will use for the argument of the function
+ * @n:  is the int that will use for the argument of the function
  * Return: 0
  */
 int _abs(int n)
