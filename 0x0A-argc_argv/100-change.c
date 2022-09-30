4218ab6c33c31063fc4a68622c85dbe45d9ed04b
muasyasila <curtissila4@gmail.com>
2022-09-30 13:55:03 -0700

    Task X Correction

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,18 +1,18 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 
-/** 
+/**
  * main - function
- * @argc: length of argv 
+ * @argc: length of argv
  * @argv: number of argument
- * Return: Always 0 
+ * Return: Always 0
  */
 
 int main(int argc, char *argv[])
 {
-	/*Declaring variables*/ 
-	int position, total, change, aux; 
-	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/ 
+	/*Declaring variables*/
+	int position, total, change, aux;
+	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
 
 	position = total = change = aux = 0;
 
@@ -37,15 +37,15 @@ int main(int argc, char *argv[])
 	{
 		if (total >= coins[position])
 		{
-			aux = (total / coins[position]); 
+			aux = (total / coins[position]);
 			change += aux;
-			total -= coins[position] * aux; 
+			total -= coins[position] * aux;
 		}
 
 		position++;
 
 	}
 
-	printf("%d\n", change); 
+	printf("%d\n", change);
 	return (0);
 }
