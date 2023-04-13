8abbeff9a6760042cfba0219c821fabb0d33a69c
muasyasila <curtissila4@gmail.com>
2023-04-13 23:02:23 +0300

    Task

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,10 +1,12 @@
-#include <math.h>
 #include "search_algos.h"
+#include <math.h>
 
+/* remember compiling math.h with gcc requires `-lm` */
 
 size_t min(size_t a, size_t b);
 
 /**
+ * min - returns the minimum of two size_t values
  * @a: first value
  * @b: second value
  *
